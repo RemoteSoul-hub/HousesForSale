e687cfa0475251ac9bc14803567a0dbca9859824
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-05-10 17:47:33 +0000

    generated service for house data / imported to home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HousesService } from '../../services/houses.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private housesService: HousesService) { }
 
   ngOnInit(): void {
+  }
+
+  getHouses() {
+    
   }
 
 }
